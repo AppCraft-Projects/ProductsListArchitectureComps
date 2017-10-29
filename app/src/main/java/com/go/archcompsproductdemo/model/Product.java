71d6634c290a5f39f3a239bd7b0d217d84313cfa
Gabor Orosz <gabor.orosz@me.com>
2017-10-29 14:25:53 +0100

    Added Product model class and GSON dependency to prepare for JSON serialization

---
+++ app/src/main/java/com/go/archcompsproductdemo/model/Product.java
@@ -0,0 +1,43 @@
+package com.go.archcompsproductdemo.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Product {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+
+    @SerializedName("name")
+    @Expose
+    private String title;
+
+    @SerializedName("thumbnailUrl")
+    @Expose
+    private String thumbnailUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
